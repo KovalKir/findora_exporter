b0f469f76e1c656de4d3d40d65d7cd33a46494b4
KovalKir <koval.kir2014@yandex.ru>
2022-07-25 21:07:03 +0300

    setting metrics by GET only, add .env

--- metrics.js
+++ metrics.js
@@ -1,37 +1,16 @@
 'use strict'
 
+const dotenv = require('dotenv');
 const client = require('prom-client');
-
-const { exec } = require('child_process');
-
 const axios = require ('axios').default;
 
-const requestInterval = 60000; //ms
+dotenv.config();
 
-const statusFn = '"is_online":';
-const balanceFn = '"bond":';
-const stakeFn = '"voting_power":';
-const rewardsFn = '"rewards":';
+const requestInterval = process.env.INTERVAL;
+const url = process.env.EXPLORER_URL;
 
-async function checkNumericMetric (expr, gauge) {
-    exec(`/usr/local/bin/fn show | grep -w '${expr}' | sed 's/.$//' | sed 's/^.*: //'`, 
-    (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
 
-        let result = Number(stdout) / 1000000;
-        gauge.set(result);
-    })
-
-}
-
-function setNumericMetrics (registry) {
+function setExplorerMetrics (registry) {
     
     const balance = new client.Gauge ({
         name: 'self_delegation_FRA',
@@ -45,50 +24,29 @@ function setNumericMetrics (registry) {
         registers: [registry],
     });
 
-    const rewards = new client.Gauge({
-        name: 'validator_rewards',
-        help: 'Avaliable Rewards (FRA)',
-        registers: [registry],
-    });
-
-    setInterval(async () => await checkNumericMetric(balanceFn, balance), requestInterval);
-    setInterval(async () => await checkNumericMetric(stakeFn,stake), requestInterval);
-    setInterval(async () => await checkNumericMetric(rewardsFn, rewards), requestInterval);
-
-}
-
-function checkValidatorStatus (registry) {
-    const gauge = new client.Gauge({
+    const status = new client.Gauge({
         name: 'validator_status',
         help: 'Validator Online Status (1 = online, 0 = offline)',
         registers: [registry],
     });
 
-    function checkStatus () { 
-        exec(`/usr/local/bin/fn show | grep -w "is_online" | sed 's/.$//' | sed 's/^.*: //'`, 
-            async (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-
-                let status = String (stdout);
-
-                status = status.trim();
-                
-                status == 'true' ? status = 1 : status = 0;
-                gauge.set(status);
-
-            })
-    }
+    function setMetrics () {
+        axios.get(url).then ( (response) => {
+        
+            balance.set(response.data.self_staking);
+            stake.set(response.data.voting_power);
+            response.data.is_online === true ? status.set(1) : status.set(0);
     
-    setInterval(checkStatus, requestInterval);
+        }).catch ( (error) => {
+            console.error(error);
+        });
+
+    }
+
+    setInterval(setMetrics, requestInterval);
 
 }
+
 
 function checkSyncNode (registry) {
     
@@ -113,7 +71,6 @@ function checkSyncNode (registry) {
 }
 
 module.exports = (registry) => {
-    setNumericMetrics(registry);
-    checkValidatorStatus(registry);
+    setExplorerMetrics(registry);
     checkSyncNode(registry);
 };
