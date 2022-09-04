18455fbe31c8292adca278855588b648fedf2de5
KovalKir <koval.kir2014@yandex.ru>
2022-09-04 13:57:33 +0300

    add statusCode

--- metrics.js
+++ metrics.js
@@ -16,29 +16,34 @@ function setExplorerMetrics (registry) {
         name: 'self_delegation_FRA',
         help: 'Validator Self Delegation (FRA)',
         registers: [registry],
+        labelNames: ['statusCode'],
     });
 
     const stake = new client.Gauge ({
         name: 'validator_voting_power',
         help: 'Total Stake (FRA)',
         registers: [registry],
+        labelNames: ['statusCode'],
     });
 
     const status = new client.Gauge({
         name: 'validator_status',
         help: 'Validator Online Status (1 = online, 0 = offline)',
         registers: [registry],
+        labelNames: ['statusCode'],
     });
 
     function setMetrics () {
         axios.get(url).then ( (response) => {
         
-            balance.set(response.data.self_staking/1000000);
-            stake.set(response.data.voting_power/1000000);
-            response.data.is_online === true ? status.set(1) : status.set(0);
+            balance.labels({statusCode: '200'}).set(response.data.self_staking/1000000);
+            stake.labels({statusCode: '200'}).set(response.data.voting_power/1000000);
+            response.data.is_online === true ? status.labels({statusCode: '200'}).set(1) : status.labels({statusCode: '200'}).set(0);
     
         }).catch ( (error) => {
-            console.error(error);
+            balance.labels({statusCode:'500'}).set(500);
+            stake.labels({statusCode:'500'}).set(500);
+            status.labels({statusCode:'500'}).set(500);
         });
 
     }
@@ -54,14 +59,15 @@ function checkSyncNode (registry) {
         name: 'validator_sync_status',
         help: 'Validator Sync Status (1 = synced, 0 = catching up)',
         registers: [registry],
+        labelNames: ['statusCode'],
     });
     
     function checkSync () {
         axios.get('http://127.0.0.1:26657/status').then ( (response) => {
             const isSynced = response.data.result.sync_info.catching_up;
-            isSynced === true ? gauge.set(0) : gauge.set(1);
+            isSynced === true ? gauge.labels({statusCode: '200'}).set(0) :  gauge.labels({statusCode: '200'}).set(1);
         }).catch( (error) => {
-            console.error(error);
+            gauge.labels({statusCode: '500'}).set(500);
         });
 
     }
